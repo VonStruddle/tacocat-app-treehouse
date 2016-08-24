acb12bb087bc4c64ee62840deb79af5208b82653
Quentin Durantay <quentin.durantay@gmail.com>
2016-08-24 21:50:56 +0200

    Added config for app init

--- tacocat.py
+++ tacocat.py
@@ -1,14 +1,40 @@
-from flask import Flask, render_template
+from flask import Flask, g, render_template, flash, redirect, url_for
+from flask.ext.bcrypt import check_password_hash
+from flask.ext.login import LoginManager, login_user, logout_user, login_required, current_user
 
+import forms
+import models
+
+
+DEBUG = True
+PORT = 8080
+HOST = '0.0.0.0'
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret'
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
 
 
 @app.route('/')
 def index():
     return render_template('index.html',
-                           tacos = ['taco1', 'taco2'])
+                           tacos=['taco1', 'taco2'])
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    models.initializeDB()
+    try:
+        models.User.create_user(
+            email='quentin.durantay@gmail.com'
+            password='passwordsohard'
+        )
+        models.User.create_user(
+            email='quentin.durantay@outlook.com'
+            password='passwordsohard2'
+        )
+    except ValueError:
+        pass
+    app.run(host=HOST, port=PORT, debug=DEBUG)
