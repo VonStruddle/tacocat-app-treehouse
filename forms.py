ef3a43c54b34de0d1bcdff2b48f86d01cff7aa54
Quentin Durantay <quentin.durantay@gmail.com>
2016-08-24 21:38:41 +0200

    Added forms for registration, login and taco creation

---
+++ forms.py
@@ -0,0 +1,55 @@
+from flask_wtf import Form
+from wtforms import StringField, PasswordField, BooleanField, TextAreaField
+from wtforms.validators import DataRequired, ValidationError, Email, Length, EqualTo
+
+
+from models import User
+
+
+def email_exists(form, field):
+    if User.select().where(User.email == field.data).exists():
+        raise ValidationError('User with that email already exists.')
+
+
+class RegisterForm(Form):
+    email = StringField(
+        'Email',
+        validators=[
+            DataRequired(),
+            Email(),
+            email_exists
+        ]
+    )
+    password = PasswordField(
+        'Password',
+        validators=[
+            DataRequired(),
+            Length(min=6),
+            EqualTo('password2', message='Passwords must match')
+        ]
+    )
+    password2 = PasswordField(
+        'Confirm password',
+        validators[DataRequired()]
+    )
+
+
+class LoginForm(Form):
+    email = StringField(
+        'Email',
+        validators=[
+            DataRequired(),
+            Email()
+        ]
+    )
+    password = PasswordField(
+        'Password',
+        validators=[DataRequired()]
+    )
+
+
+class TacoForm(Form):
+    protein = CharField('Protein')
+    cheese = BooleanField('Cheese')
+    shell = CharField('Shell')
+    extras = TextAreaField('Extras')
