3ca8e21fabe9037d8271fed979857b1eac13a083
Quentin Durantay <VonStruddle@users.noreply.github.com>
2016-08-26 13:49:30 +0200

    Update forms.py

--- forms.py
+++ forms.py
@@ -2,7 +2,6 @@ from flask_wtf import Form
 from wtforms import StringField, PasswordField, BooleanField, TextAreaField
 from wtforms.validators import DataRequired, ValidationError, Email, Length, EqualTo
 
-
 from models import User
 
 
